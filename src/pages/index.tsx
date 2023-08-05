8bd65ddb502a96b4fbdec6eb4646aa4398affd3f
Miles <miles@gmail.com>
2023-08-04 17:29:28 -0700

    this stupid chaining error is pissing me off, fix build?

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,8 +25,10 @@ const CreatePostWizard = () => {
     },
     onError: (e) => {
       const errorMessage = e.data?.zodError?.fieldErrors.content;
-      if (errorMessage && errorMessage[0]) {
-        toast.error(errorMessage[0]);
+      if (errorMessage) {
+        if (errorMessage[0]) {
+          toast.error(errorMessage[0]);
+        }
       } else {
         toast.error("Failed to post! Please try agan later.");
       }
